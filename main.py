efba42fdd1b106d712e983d325653d7edd2f3315
Aditya <lapratomo24@yahoo.com>
2025-01-27 22:33:07 +0100

    added menu display

--- main.py
+++ main.py
@@ -1,15 +1,75 @@
 import sys
-import csv
 import time
 import os
 import platform
 import random
 from colorama import init, Fore, Style
-from quiz import Quiz
+from quiz import Quiz, DATA_DIR, FILE_PATH
 
 init(autoreset=True)
 
-# Constants
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
-FILE_PATH = os.path.join(DATA_DIR, "trivia.csv")
 
+def ensure_data_dir_exists():
+    ''' Ensure data directory exists '''
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+
+
+def clear_screen():
+    ''' Clears the terminal screen '''
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def loading():
+    ''' Displays a short loading animation '''
+    print("Now loading", end="", flush=True)
+    for _ in range(3):
+        time.sleep(0.3)
+        print(".", end="", flush=True)
+    clear_screen()
+    
+
+def mode_validation(input):
+    ''' Validates user input for mode selection '''
+    if input in ["a", "b", "c"]:
+        return True
+    else:
+        print(Fore.RED + "Invalid input. Try again" + Style.RESET_ALL)
+        return False
+
+
+def display_menu():
+    ''' Displays the main menu '''
+    print(Fore.BLUE + "\nWelcome to Video Game Trivia!")
+    print("Select a mode to run the program:\n" + Style.RESET_ALL)
+    print("=====================================")
+    print("(a) Trivia Explorer")
+    print("(b) Trivia Quiz")
+    print("(c) Exit")
+    print("=====================================")
+
+
+def main():
+    """Runs all functions"""
+    ensure_data_dir_exists()
+    
+    while True:
+        display_menu()
+        select = input("\nSelect (a) or (b) to run the program: ").strip().lower()
+        if mode_validation(select):
+            loading()
+            
+            if select == "a":
+                run_trivia_explorer()
+            elif select == "b":
+                run_trivia_quiz()
+            else:
+                print("Exiting program...")
+                sys.exit(1)
+    
+
+if __name__ == "__main__":
+    main()
--- quiz.py
+++ quiz.py
@@ -1,19 +1,29 @@
+import csv
+import os
+import random
 from quizzes import quizzes
+from datetime import datetime
 from colorama import init, Fore, Style
 
 init(autoreset=True)
 
+# Constants
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+FILE_PATH = os.path.join(DATA_DIR, "score.csv")
+
+
 class Quiz:
-    ''' Stores questions and answers '''
+    """Stores questions and answers"""
+
     def __init__(self, questions):
         self.questions = questions
         self.score = 0
-        
+
     def display_question(self, question_data):
         print(question_data["question"])
         for i, option in enumerate(question_data["options"], start=1):
             print(f"{i}. {option}")
-    
+
     def get_answer(self):
         while True:
             try:
@@ -24,15 +34,19 @@ class Quiz:
                     print("Please select a number between 1 and 3")
             except ValueError:
                 print("Invalid input. Please enter a number")
-    
+
     def check_answer(self, question_data, user_input):
-        correct_answer =  question_data["options"].index(question_data["answer"]) + 1    
+        correct_answer = question_data["options"].index(question_data["answer"]) + 1
         if user_input == correct_answer:
             print(Fore.GREEN + "\nCorrect answer!\n" + Style.RESET_ALL)
             self.score += 1
         else:
-            print(Fore.RED + f"\nWrong answer. The correct answer is {question_data["answer"]}.\n" + Style.RESET_ALL)
-    
+            print(
+                Fore.RED
+                + f"\nWrong answer. The correct answer is {question_data["answer"]}.\n"
+                + Style.RESET_ALL
+            )
+
     def run_quiz(self):
         print("Welcome to the Video Game Trivia Quiz!\n")
         for question in self.questions:
@@ -40,7 +54,7 @@ class Quiz:
             user_choice = self.get_answer()
             self.check_answer(question, user_choice)
         print(f"Quiz over! Your final score is {self.score} / {len(self.questions)}")
-        
-    
+
+
 quiz = Quiz(quizzes)
 quiz.run_quiz()
